891e2e14d0d7f6da217750f243bebc435b980978
babamba <orochi13@naver.com>
2020-08-18 00:44:51 +0900

    refactor: :sparkles: 스크랩만 보기 버튼 구성

--- src/components/Main/FavoriteSelect.tsx
+++ src/components/Main/FavoriteSelect.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import CheckedIcon from "../../assets/images/bt-checkbox-checked.png";
+import DefailtIcon from "../../assets/images/white.png";
+import styled from "styled-components";
+import { observer } from "mobx-react";
+import useStores from "../../hooks/useStores";
+
+const Container = styled.div`
+  cursor: pointer;
+`;
+const CheckImage = styled.img`
+  padding-right: 6px;
+`;
 
 const FavoriteSelect = () => {
-  return <div>FavoriteSelect</div>;
+  const { bookStore } = useStores();
+
+  const handleClick = () => {
+    bookStore.OnlyBookMarkList(!bookStore.OnlyBookMark);
+  };
+
+  return (
+    <Container onClick={() => handleClick()}>
+      <CheckImage src={bookStore.OnlyBookMark ? CheckedIcon : DefailtIcon} />
+      스크랩한 것만 보기
+    </Container>
+  );
 };
 
-export default FavoriteSelect;
+export default observer(FavoriteSelect);
