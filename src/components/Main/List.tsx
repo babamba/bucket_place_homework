3d58bbb043dc2f2b0f4ecda552c3723d95912c03
babamba <orochi13@naver.com>
2020-08-18 00:46:21 +0900

    refactor: :art: List Component 작성

--- src/components/Main/List.tsx
+++ src/components/Main/List.tsx
@@ -1,35 +1,105 @@
-import React, { useState, useEffect } from "react";
-import { Row, Col } from "antd";
+import React, { useState, useEffect, useRef } from "react";
+import { Row, Col, Spin, Skeleton } from "antd";
 import styled from "styled-components";
+import { observer } from "mobx-react";
 import useStores from "../../hooks/useStores";
 import Card from "./Card";
-// import InfiniteScroll from "react-infinite-scroller";
+import { Content } from "../../interface/content";
+import InfiniteScroll from "react-infinite-scroller";
+// import { LoadingOutlined } from "@ant-design/icons";
 
 const Container = styled.div``;
+const SpinBox = styled.div`
+  height: 200px;
+  text-align: center;
+`;
 
 const List = () => {
-  const { bookStore } = useStores();
+  const scrollParentRef = useRef<HTMLDivElement>(null);
+  const [cardList, setList] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const {
+    bookStore: { getRooms, roomlist, AddPageNum, hasMore },
+  } = useStores();
 
   useEffect(() => {
-    const result = bookStore.getRooms(1);
-    console.log("result : ", result);
+    handleFetch(1);
   }, []);
+
+  useEffect(() => {
+    if (roomlist.length > 0) handleRender();
+  }, [roomlist]);
+
+  const handleFetch = async (page: number) => {
+    const result = await getRooms(page);
+    if (result) {
+      handleRender();
+    }
+  };
+
+  const handleRender = async () => {
+    let list = roomlist.map((item: Content, idx: number) => {
+      return <Card content={item} key={idx} />;
+    });
+
+    setList(list);
+  };
+
+  // const SkeletonShape = () => (
+  //   <Col xs={24} sm={12} md={8} lg={6} xl={6} xxl={6}>
+  //     <Row>
+  //       <Col>
+  //         <Skeleton.Avatar active size={36} shape="circle" />
+  //         <Skeleton.Input style={{ width: 120, marginLeft: 8 }} active />
+  //       </Col>
+  //     </Row>
+  //   </Col>
+  // );
+
+  const handleInfiniteOnLoad = async () => {
+    if (hasMore) {
+      console.log("loadmore");
+      const AddNum = await AddPageNum();
+      await handleFetch(AddNum);
+      // setLoading(true);
+
+      // setTimeout(async () => {
+      //   const AddNum = await AddPageNum();
+      //   await handleFetch(AddNum);
+      //   setLoading(false);
+      // }, 1000);
+    }
+  };
+
   return (
-    <Container>
-      <Row>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </Row>
+    <Container className="ScrollDiv" ref={scrollParentRef}>
+      <InfiniteScroll
+        initialLoad={false}
+        pageStart={0}
+        loadMore={handleInfiniteOnLoad}
+        hasMore={hasMore}
+        useWindow={true}
+        threshold={150}
+        // loader={
+        //   <SpinBox key={0}>
+        //     <Spin
+        //       indicator={<LoadingOutlined style={{ fontSize: 24 }} />}
+        //       spinning={loading}
+        //     />
+        //     <Row>
+        //       <SkeletonShape />
+        //       <SkeletonShape />
+        //       <SkeletonShape />
+        //       <SkeletonShape />
+        //     </Row>
+        //   </SpinBox>
+        // }
+      >
+        <Row>{cardList}</Row>
+      </InfiniteScroll>
     </Container>
   );
 };
 
-export default List;
+export default observer(List);
