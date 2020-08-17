8aed2f88aad1530abdd225bd4443d00d03b57352
babamba <orochi13@naver.com>
2020-08-18 00:43:46 +0900

    refactor: :sparkles: 어플리케이션 진입시 로컬스토리지 컨디션 체크

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Routes from "./route/Route";
+import useStores from "./hooks/useStores";
 import "./App.css";
 
 const Container = styled.div`
@@ -10,6 +11,15 @@ const Container = styled.div`
 `;
 
 function App() {
+  const {
+    bookStore: { LocalCheck },
+  } = useStores();
+
+  // 최초 어플리케이션 진입시 로컬스토리지가 null 상태인지 체크
+  useEffect(() => {
+    LocalCheck();
+  }, []);
+
   return (
     <Container>
       <Routes />
